f7969fc6969f18736b449b4607921c92687ef2e4
feelflowofficial <feelflow.official@gmail.com>
2023-11-08 22:28:44 +0900

    codingTest commit!!!!!!!

---
+++ src/main/java/softeer/Softeer_Level_2_5.java
@@ -0,0 +1,23 @@
+package softeer;
+
+import java.util.Scanner;
+
+public class Softeer_Level_2_5 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int N = scanner.nextInt();
+
+        int j = 2;
+        // 2 3 5 9 17 33
+
+        int count = 0;
+
+        // 2 * 2,  3 * 3, 5 * 5, 9 * 9
+        for (int i = 0; i < N; i++) {
+            j = j + j -1;
+            count = j * j;
+        }
+        System.out.println(count);
+    }
+}
